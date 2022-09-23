cb6368672af78e6c95cfe6c8cb0d08bf87d964da
Akash kumar <81312989+Akkii97@users.noreply.github.com>
2022-09-24 02:23:59 +0530

    Create Median of two sorted arrays.java
    Algorithm
    
    1. First we will take another array of the length of the sum of these two arrays.
    2. Then we will try to sort the elements of both arrays by comparing the elements of these arrays and storing them in a new array.
    3. This process will be continued till elements of one array are sorted.
    4. Then we will check for the remaining elements of the other array and store them in a new array.
    5. After that we will check whether the total number of elements are odd or even, according to that we will find median.

---
+++ Median of two sorted arrays.java
@@ -0,0 +1,45 @@
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+class Main {
+     static double median(int arr1[],int arr2[],int a,int b) {
+        int finalArray[]=new int[a+b];
+        int i=0,j=0,k=0;
+        while(i<a && j<b) {                           // This is used to sort the elements between both arrays and store them in 3rd array
+            if(arr1[i]<arr2[j]) {
+                finalArray[k++] = arr1[i++];
+            }
+            else {
+                finalArray[k++] = arr2[j++];
+            }
+        }
+        if(i<a) {                                     // If after sorting elements are left in 1st array, then we will store them
+            while(i<a)
+                finalArray[k++] = arr1[i++];
+        }
+        if(j<b) {                                     // If after sorting elements are left in 1st array, then we will store them
+            while(j<b)
+                finalArray[k++] = arr2[j++];
+        }
+        b = a+b;
+        if(b%2==1)
+            return finalArray[((b+1)/2)-1];
+        else return ((double)finalArray[(b/2)-1]+(double)finalArray[(b/2)])/2.0;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();  // length for 1st array
+        int b = sc.nextInt();  // length for 2nd array
+        int arr1[] = new int[a];
+        int arr2[] = new int[b];
+        for (int i=0; i<a; i++){    // initializing the elements in 1st array
+            arr1[i]=sc.nextInt();
+        }
+        for (int i=0; i<b; i++){   // initializing the elements in 2nd arrray
+            arr2[i]=sc.nextInt();
+        }
+       // System.out.print("The Median of two sorted array is ");
+        System.out.printf("%.2f",median(arr1,arr2,a,b));  // calling the median function
+    }
+}
